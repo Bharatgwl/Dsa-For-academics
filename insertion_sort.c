d870007c7b0793ecf35a2a2bbe68db4a828f3810
Bharatgwl <156042634+Bharatgwl@users.noreply.github.com>
2024-09-18 08:38:53 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -3,13 +3,13 @@ void insertion_sort(int arr[])
 {
     for (int i = 1; i < 5; i++)
     {
-        int min_index = arr[i];
+        int min = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > min_index){
+        while (j >= 0 && arr[j] > min){
             arr[j+1]=arr[j];
             j--;
         }
-        arr[j+1]=min_index;
+        arr[j+1]=min;
     }
 }
 int main()
